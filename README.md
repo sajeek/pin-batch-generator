0d81f3799575609e79ca6acb3e792a68c889ac94
Sajee Karunaratne <sajeepra@gmail.com>
2023-08-05 11:04:43 +1200

    refactor

--- README.md
+++ README.md
@@ -11,6 +11,6 @@ Steps to run =>
 - run tests - mvn test
 - surefire report - mvn surefire:test
 
-Assumptions =>
+Limitations =>
 ---------------
 - Library assures 1000 unique pins in each batch. If it is executed for X number of times to get (1000 * X) pins, certainly there will be duplicates.
--- src/main/java/com/wp/common/Util.java
+++ src/main/java/com/wp/common/Util.java
@@ -7,13 +7,13 @@ import java.util.Set;
 public class Util {
 	
 	private static final Random random = new Random();
-	private static final String PIN_FORMATTER = "%04d";
+	private static final String PIN_FORMAT_PATTERN = "%04d";
 	private static final short SHORT_10000 = 10000;
 	
 	public Set<String> generatePinBatch() {
 		var randomPins = new HashSet<String>();
 		while (randomPins.size() < 1000)
-			randomPins.add(String.format(PIN_FORMATTER, random.nextInt(SHORT_10000)));
+			randomPins.add(String.format(PIN_FORMAT_PATTERN, random.nextInt(SHORT_10000)));
 		return randomPins;
 	}
 }
